7923f39dea43e97c4847ada00768dc4b679967e2
damfinkel <damfinkel@hotmail.com>
2015-08-01 18:34:04 -0300

    rEINITIALIZE

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  #ojo!! ver de sacar esto despues!!!
+  skip_before_action :verify_authenticity_token
+
+  def login
+  	render_invalid_parameters and return if params[:token].blank? || params[:name].blank? 
+  	@user = User.find_by(name: params[:name])
+  	
+  	@user = User.create(name: params[:name], token: params[:token]) if @user.nil?
+  	
+		if @user.token.blank?
+			@user.update(token: params[:token])
+			render_ok and return
+		end
+		render_ok and return if @user.token.eql?(params[:token])
+		render_other_problems and return
+		
+
+  end
+
+	def logout
+  	render_invalid_parameters and return if params[:token].blank? || params[:name].blank? 
+  	@user = User.find_by(name: params[:name])
+  	
+  	render_invalid_parameters if @user.nil?
+  	
+		if @user.token.eql?(params[:token])
+			@user.update(token: nil)
+			render_ok and return 
+		else
+			render_other_problems and return
+		end
+  end  
+
+  def update_position
+   	render_invalid_parameters and return unless validate_parameters?
+   	@user = User.find_by(name: params[:name])
+   	if @user.token.eql?(params[:token])
+			@user.update(latitude: params[:latitude],longitude: params[:longitude])
+			render_ok and return 
+		else
+			render_other_problems and return
+		end
+  end
+
+
+  def validate_parameters?
+  	empty_parameters = (params[:name].blank? || params[:token].blank? || params[:latitude].blank? || params[:longitude].blank?) 
+  	valid_types = (float?(params[:latitude]) && float?(params[:longitude]))
+  	return !empty_parameters && valid_types
+  end
+
+  def float? object
+  	!!Float(object) rescue false
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetar::Application.config.secret_key_base = 'b39e6c84dca43ba1c749e400376444772fb3b750c1272fdfc503df636e066eb423467785edaeda1a095d60818c0fda9074fd90e86d2a3be31ef2bd833800f5a3'
